0e50e3c06c2c1d4aaadbae808a264ac54c954a9c
axce-j <131579572+axce-j@users.noreply.github.com>
2023-06-30 10:22:25 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const menu = document.querySelector('#menu');
+const element = document.querySelector('#nav');
+
+
+
+menu.addEventListener('click', () => {
+
+    element.classList.toggle("box");
+})
+
